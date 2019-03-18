d4d81b03d5b8eebc6fd5e3aa0cd0cf0a5e7a51f5
Jayanth Mouli <45921490+JayanthMouli@users.noreply.github.com>
2019-03-18 09:12:06 -0700

    Update netcdfreader.py

--- netcdfreader/netcdfreader.py
+++ netcdfreader/netcdfreader.py
@@ -1,3 +1,5 @@
+#convenience print variables from nc file
+
 from netCDF4 import Dataset
 dset = Dataset('/home/jayanthmouli/Desktop/fire/netcdfreader/FWI.MERRA2.CORRECTED.Daily.Default.20150131.nc')
 print dset.variables
