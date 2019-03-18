353ace2a6e214a10525d2f2b189bd5a9082d6796
Jayanth Mouli <45921490+JayanthMouli@users.noreply.github.com>
2019-03-18 09:25:28 -0700

    Update and rename codeminmax.py to normalize.py

--- regression/normalize.py
+++ regression/normalize.py
@@ -1,3 +1,5 @@
+#NORMALIZE FWI VALUES IN INITIAL CSV
+
 import csv
 import datetime
 import numpy as np
@@ -5,7 +7,7 @@ import os
 from netCDF4 import Dataset
 import pandas
 
-
+#INITIALIZE REGULAR LISTS
 fsize = []
 dc = []
 dmc = []
@@ -13,7 +15,7 @@ ffmc = []
 isi = []
 bui = []
 fwi = []
-
+#INITIALIZE NORMALIZED LISTS
 fsizenorm = []
 dcnorm = []
 dmcnorm = []
@@ -21,9 +23,11 @@ ffmcnorm = []
 isinorm = []
 buinorm = []
 fwinorm = []
+#OPEN INPUT CSV
 with open('fwi2015firespropcols.csv', 'r') as csvinput:
 	reader = csv.reader(csvinput)
 	reader.next()
+	#ADD VARIABLES TO LISTS
 	for row in reader:
 		fsize.append(float(row[0]))
 		dc.append(float(row[1]))
@@ -32,7 +36,7 @@ with open('fwi2015firespropcols.csv', 'r') as csvinput:
 		isi.append(float(row[4]))
 		bui.append(float(row[5]))
 		fwi.append(float(row[6]))
-	
+#FIND MAXIMUM VALUES 	
 MAX_fsize = max(fsize)
 MAX_dc = max(dc)
 MAX_dmc = max(dmc)
@@ -40,7 +44,7 @@ MAX_ffmc = max(ffmc)
 MAX_isi = max(isi)
 MAX_bui = max(bui)
 MAX_fwi = max(fwi)
-
+#FIND MINIMUM VALUES
 MIN_fsize = min(fsize)
 MIN_dc = min(dc)
 MIN_dmc = min(dmc)
@@ -48,7 +52,7 @@ MIN_ffmc = min(ffmc)
 MIN_isi = min(isi)
 MIN_bui = min(bui)
 MIN_fwi = min(fwi)
-
+#USE FORMULA Xnorm = (x-MIN)/(MAX-MIN) TO FIND NORMALIZED VALUES, APPEND VALUES TO LISTS
 for F_S in range(0, len(fsize)):
 	fsizenorm.append(((fsize[F_S] - MIN_fsize) / (MAX_fsize - MIN_fsize)))
 for d in range(0, len(dc)):
@@ -63,7 +67,7 @@ for b in range(0, len(bui)):
 	buinorm.append(((bui[b] - MIN_bui) / (MAX_bui - MIN_bui)))
 for fw in range(0, len(fwi)):
 	fwinorm.append(((fwi[fw] - MIN_fwi) / (MAX_fwi - MIN_fwi)))
-
+#CREATE NEW DATASET FROM NORMALIZED VALUES, SAVE AS CSV
 raw_data = { 'FIRE_SIZE':fsizenorm,'DC':dcnorm, 'DMC':dmcnorm, 'FFMC':ffmcnorm,'ISI':isinorm,'BUI':buinorm, 'FWI':fwinorm}
 DF = pandas.DataFrame(raw_data, columns = ['FIRE_SIZE', 'DC', 'DMC', 'FFMC', 'ISI', 'BUI', 'FWI'])
 DF.to_csv('fwi2015firesnormalized.csv')	
